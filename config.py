5cd1285d96f3e036e423ae393dcc6fb092953308
Dr.bin <1436956920@qq.com>
2026-01-02 02:50:04 +0800

    恢复原始pl0
    恢复原始pl0

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ Configuration File - 配置文件
 DEFAULT_COMPILER = "generated/compiler.py"
 
 # 默认源文件夹路径
-DEFAULT_SOURCE_DIR = "examples/pl0_subset/programs"
+DEFAULT_SOURCE_DIR = "examples/error_test/pl0"
 #examples/pl0_subset/programs
 #examples/error_test/pl0
 
